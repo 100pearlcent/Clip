1fd64258ef40812ee51bd7f0c8abe290647a62a9
100pearlcent <100pearlcent@naver.cm>
2021-07-14 13:03:27 +0900

    ADD Title Text Component of Mainpage

---
+++ src/components/atoms/Text/MainTitle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+/* 메인페이지 타이틀 텍스트 */
+let MainTitle = styled.div`
+    font-family: "Exo2-SemiBold";
+    color: ${props=> props.theme.black};
+    font-size: 4rem;
+`;
+
+export default MainTitle;
