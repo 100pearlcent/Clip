9cf52e749e9336c154ab5e4800b2e002860f7d2f
100pearlcent <100pearlcent@naver.cm>
2021-07-17 20:11:20 +0900

    ADD Author Atom

---
+++ src/components/atoms/Text/Author.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+let Author = styled.p`
+    font-family: "BebasNeue-Regular";
+    color: ${props=> props.theme.gray};
+    font-size: 8vh;
+`;
+
+export default Author;
