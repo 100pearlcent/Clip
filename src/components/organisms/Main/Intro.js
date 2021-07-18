7539d8a85a7d236fd9645f3e3deb9b9534333557
100pearlcent <100pearlcent@naver.cm>
2021-07-18 19:09:41 +0900

    ADD Intro Component

---
+++ src/components/organisms/Main/Intro.js
@@ -0,0 +1,16 @@
+import Header from "../../molecules/Main/Header";
+import Thumbnail from "../../molecules/Main/Thumbnail";
+
+const fit = {
+    width: '100vw',
+    height: '100vh',
+}
+
+function Intro() {
+    return <section style={fit}>
+    <Header></Header>
+    <Thumbnail></Thumbnail>
+    </section>
+};
+
+export default Intro;
