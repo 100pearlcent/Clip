8f717ee2700fb46c3628b25ece84d80007962172
100pearlcent <100pearlcent@naver.cm>
2021-07-18 14:15:06 +0900

    ADD Intro Page

---
+++ src/pages/IntroPage.js
@@ -0,0 +1,15 @@
+import Intro from '../components/organisms/Main/Intro';
+import FirstContent from '../components/organisms/Main/FirstContent';
+import SecondContent from '../components/organisms/Main/SecondContent';
+import Outro from '../components/organisms/Main/Outro';
+
+function IntroPage() {
+    return <>
+    <Intro/>
+    <FirstContent/>
+    <SecondContent/>
+    <Outro/>
+    </>
+}
+
+export default IntroPage;
