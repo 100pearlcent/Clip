f5a1b0c49f7674ba7a1e9e0f6176ee47c69552c9
100pearlcent <100pearlcent@naver.cm>
2021-07-20 11:06:06 +0900

    ADD Darkmode Button Atom

---
+++ src/components/atoms/Button/DarkmodeBtn.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  return (
+    <DarkModeToggle
+      onChange={setIsDarkMode}
+      checked={isDarkMode}
+      size={80}
+    />
+  );
+};
