7322083b97a21b4461d4c2eabcbb8b5f9aa9c807
100pearlcent <100pearlcent@naver.cm>
2021-07-21 15:52:19 +0900

    ADD DashboardTitle Text Atom

---
+++ src/components/atoms/Text/DashboardTitle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+let DashboardTitle = styled.span`
+    font-family: "LeagueSpartan-Bold";
+    font-size: 4.5vh;
+    color: ${props=> props.theme.dimGray};
+`
+
+export default DashboardTitle;
