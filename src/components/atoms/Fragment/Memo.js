0c02d093e8333fd3b0ce394c0f87ca723e5a9224
100pearlcent <100pearlcent@naver.cm>
2021-07-28 14:26:49 +0900

    ADD Memo Atom (Sticky Note)

---
+++ src/components/atoms/Fragment/Memo.js
@@ -0,0 +1,14 @@
+const style={
+    borderRadius: '20px',
+    width: '30vh',
+    height: '30vh',
+    background: '#FEFF9C'
+}
+
+function Memo() {
+    return <div style={style}>
+
+    </div>
+}
+
+export default Memo;
