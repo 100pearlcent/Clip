ae5c3af4c3b3ca8bea47600ef7861c09e9a1b7af
100pearlcent <100pearlcent@naver.cm>
2021-08-17 03:50:02 +0900

    ADD Weather-related Atom Components

---
+++ src/components/atoms/Text/Temperature.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+let Temperature = styled.div `
+font-size: 10vh;
+font-family: "LeagueSpartan-Bold";
+text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15), -1px 0px 6px rgba(206,89,55,0);
+color: #fff;
+`;
+
+export default Temperature;
