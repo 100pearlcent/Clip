d09b0c2c3d362b956d53ee66dbe6c91882892f2c
100pearlcent <100pearlcent@naver.cm>
2021-08-17 03:42:26 +0900

    ADD Left Side Bar Organism Component

---
+++ src/components/organisms/Dashboard/LeftSideBar.js
@@ -0,0 +1,25 @@
+import SideBtnSet from "../../molecules/Dashboard/SideBtnSet";
+// import DarkmodeBtn from "../../atoms/Button/DarkmodeBtn";
+
+const style= {
+    width: '15vh',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'white'
+}
+
+const divider= {
+    height: '30vh'
+}
+
+function LeftSideBar() {
+    return <div style={style}>
+        <SideBtnSet/>
+        <div style={divider}/>
+    </div>
+}
+
+export default LeftSideBar;
