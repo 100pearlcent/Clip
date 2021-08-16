e1ffd660fc3ad9f1055970a4d7bb9d8c0304c7d2
100pearlcent <100pearlcent@naver.cm>
2021-08-17 03:47:53 +0900

    ADD global color file

---
+++ src/globalStyles/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+    white: "#FFFFFF",
+    lightBlue: "#A4EBF3",
+    blue: "#39A9CB",
+    red: "#FF384A",
+    lightGray: "#EFF3F9", // 배경색
+    brightGray: "#E5E5E5",
+    gray: "#AAAAAA",
+    dimGray: "#4F4F4F",
+    darkGray: "#000000AD",
+    calGray: "#666666",
+    black: "#0D0D0D",
+    yellow: "#FFEDA3"
+};
+
+export { colors };
